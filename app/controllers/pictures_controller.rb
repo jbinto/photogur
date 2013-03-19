a6ad79d4d370d1e7f84bf11541729cb222c061ff
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-19 15:44:47 -0400

    Show some pictures (hardcoded in the controller for now)

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,11 @@
 class PicturesController < ActionController::Base
 	def index
+    @greeting = "Hello world"
 
+    @pictures = [
+      'http://monicao.s3.amazonaws.com/bitmaker/house.jpg',
+      'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg',
+      'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg'
+    ]
 	end
 end
