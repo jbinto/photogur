a1068b1178ad6c8951e3cd1b92776c917952ac50
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-20 14:46:43 -0400

    Add create action. Everything is going to the database now.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,43 +1,24 @@
 class PicturesController < ApplicationController
 
-  before_filter :load_pictures
-
-  def load_pictures
-    @pictures = [
-      {
-        title: "The old church on the coast of White sea",
-        artist: "Sergey Ershov",
-        url: 'http://monicao.s3.amazonaws.com/bitmaker/house.jpg'
-      },
-
-      {
-        title: "Sea Power",
-        artist: "Stephen Scullion",
-        url: 'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg'
-      },
-      
-      {
-        title: "Into the Poppies",
-        artist: "John Wilhelm",
-        url: 'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg'
-      }
-    ]
-  end
-
   def index
-    @greeting = "Photogur"
+    @pictures = Picture.all
   end
 
   def new
-
   end
 
   def create
-    render :text => "You posted! #{params}"
+    p = Picture.new
+    p.title = params[:title]
+    p.artist = params[:artist]
+    p.url = params[:url]
+    p.save
+
+    redirect_to pictures_path
   end
 
   def show
-    id = params[:id].to_i
-    @picture = @pictures[id]
+    id = params[:id]
+    @picture = Picture.find(id)
   end
 end
