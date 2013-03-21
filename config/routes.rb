a01011d63db8c0a84a8e5c6c8f5e49c6b72fe792
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-21 09:12:24 -0400

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Photogur::Application.routes.draw do
 
+  root :to => "pictures#index"
   get 'pictures' => 'pictures#index'
   get 'pictures/new' => 'pictures#new'
   get 'pictures/:id'  => 'pictures#show'
