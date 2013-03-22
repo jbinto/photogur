0f9caf8a2697fb78b6dad9b3284ff52baca6fb9c
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-22 09:05:24 -0400

    Add validation for URLs

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,17 @@
 class Picture < ActiveRecord::Base
 	attr_accessible :title, :artist, :url, :thumbnail_url
 	validates :title, :presence => true
-	validates :url, :presence => true
+	validates :url, 
+		:presence => true,
+		:format => { 
+			:with => /^https?:\/\/[^\s]+$/i,
+			:message => "Please enter a valid URL." 
+		}
+
+	validates :thumbnail_url, 
+		:presence => true,
+		:format => { 
+			:with => /^https?:\/\/[^\s]+$/i,
+			:message => "Please enter a valid URL." 
+		}
 end
